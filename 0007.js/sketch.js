9040bdab339d5770404464d685a9f498b729b3ef
vineetarath <vineetarath@gmail.com>
2018-05-08 23:32:14 +0530

    0007: Elliptical Orbit
    0 :)

---
+++ 0007.js/sketch.js
@@ -0,0 +1,17 @@
+var x, y, r, A
+function setup(){
+createCanvas (500,500);
+background (67);
+r1 = 240;
+r2 = 50;
+A = 20;
+//let central location be 250,250, so x = 250, y = 250;
+}
+function draw (){
+    clear ();
+    background (67);
+    ellipse(250,250,20,20);//centre
+    //x = r cos a and y = r sin a  as per trigonometry
+    A = A+25;
+    ellipse((250+ r1*cos(A)),(250+r2*sin(A)),25,25);
+}
