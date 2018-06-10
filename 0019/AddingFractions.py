7cfc12dd57a47bd29aaff2c2d5c018d0d1729963
vineetarath <vineetarath@gmail.com>
2018-06-10 17:43:57 +0530

    Adding Fractions
     :)

---
+++ 0019/AddingFractions.py
@@ -0,0 +1,8 @@
+print ("Fractions are fun! Lets add two fractions")
+an = int(input("Enter numerator of first number: "))
+ad = int(input("Enter denominator of first number: "))
+bn = int(input("Enter numerator of second number: "))
+bd = int(input("Enter denominator of second number: "))
+sn = (an*bd + bn*ad)
+sd = (ad*bd)
+print ("fraction sum =" + (str(sn)) + '''/''' + (str(sd)))#Yippee!
