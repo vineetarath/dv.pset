e020fcb8c129ff434c95234f7694541bfbe89f8c
vineetarath <vineetarath@gmail.com>
2018-06-26 00:04:40 +0530

    0082: Star Pattern 2
    Tweaks

--- 0082/StarPattern2.py
+++ 0082/StarPattern2.py
@@ -3,7 +3,7 @@ for i in range (t,0,-1):
     for j in range (i,t+1):
         print ('*', end=' ')
     print (' ')
-for i in range (t,0,-1):
+for i in range (t-1,0,-1):
     for k in range (i,0,-1):
         print ('*', end=' ')
     print (' ')
