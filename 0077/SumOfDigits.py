06e42ef22a1c3c29f402ec07858dacd14b30ea98
vineetarath <vineetarath@gmail.com>
2018-06-26 23:48:18 +0530

    0077: SumOfDigits.py
    tweaks

--- 0077/SumOfDigits.py
+++ 0077/SumOfDigits.py
@@ -1,3 +1,3 @@
-n = str(input('Enter any number between 1 and 9999 to find the sum of its digits: '))
-s = n[0]+n[1]+n[2]+n[3]
-print n
+n = int(input('Enter any number between 1 and 9999 to find the sum of its digits: '))
+v = (n[0]) + str(n[1]) + str(n[2]) + str(n[3])
+print (n[0]+ " + " + n[1]+ " + "+ n[2]+ " + "+ n[3] + " =" + str(value))
