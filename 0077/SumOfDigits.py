d945c8de15d0717f6be0fe220c3e1d1c084bc496
vineetarath <vineetarath@gmail.com>
2018-06-27 21:55:14 +0530

    0077: Sum Of Digits
    Works for 4 digit numbers

--- 0077/SumOfDigits.py
+++ 0077/SumOfDigits.py
@@ -1,3 +1,4 @@
-n = int(input('Enter any number between 1 and 9999 to find the sum of its digits: '))
-v = (n[0]) + str(n[1]) + str(n[2]) + str(n[3])
-print (n[0]+ " + " + n[1]+ " + "+ n[2]+ " + "+ n[3] + " =" + str(value))
+n = str(input('Enter any number between 0001 and 9999 to find the sum of its digits: '))
+v = int(n[0]) + int(n[1]) + int(n[2]) + int(n[3])
+#print (v)
+print (n[0]+ " + " + n[1]+ " + "+ n[2]+ " + "+ n[3] + " = " + str(v))
