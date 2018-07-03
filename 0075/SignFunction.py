b96b320eb3febfe293d0f127205ea8fef797d8fd
vineetarath <vineetarath@gmail.com>
2018-07-03 22:15:27 +0530

    0075: Sign Function
    Can't believe this sum is part of pset - saved me for one more day of struggle - just about breathing...

---
+++ 0075/SignFunction.py
@@ -0,0 +1,6 @@
+#Write sign function: returns +1 or -1 based on the sign of argument passed.
+n = int(input('Enter any number to check its sign: '))
+if n<0:
+    print ("-1")
+else:
+    print ("+1")
