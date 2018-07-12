98d01a2384180772a705bb38cba5e95a3ec77017
vineetarath <vineetarath@gmail.com>
2018-07-12 23:22:15 +0530

    0089: Base Conversion
    Not working...dying but fighting :|

---
+++ 0089/BaseConv.py
@@ -0,0 +1,9 @@
+remainderlist = []
+bten = int(input("Input:")) #get input
+while (remainder/2 != 0 or remainder/2 != 1):
+    for i in range(bten): #define range
+        remainder = bten%2
+        print (remainder)
+print (remainderlist.append(rem))
+new = list.reverse(remainderlist)
+print ("binary is: "+ new)
