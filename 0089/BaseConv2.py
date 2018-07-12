6811b020a8abd90e2af9ce32eae2951cb4c1cbc2
vineetarath <vineetarath@gmail.com>
2018-07-13 00:10:00 +0530

    still tweaking

--- 0089/BaseConv2.py
+++ 0089/BaseConv2.py
@@ -1,15 +1,17 @@
 remainderlist = [] #list of remainders whose reverse order will give the binary number
+quotientlist = []
 n = int(input("Input:")) #get denary number input
 #while (remainder/2 != 0 or remainder/2 != 1):
-q = 0
-r = 0
-for i in range (n):
-        q = n//2
-        r = n%2
-        q = q//2
-print (q,r)
+q = n//2
+r = n%2
+        #print(q)
+        #q = q//2
+quotientlist.append(q)
 remainderlist.append(r)
+print (quotientlist)
 print (remainderlist)
+
+
 #    for i in range(0,bten): #define range
 #        for j in range (0,i):
 #            quotient = bten//2
