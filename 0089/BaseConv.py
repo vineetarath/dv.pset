3b2541406412f02ec1f6c26029b661c269be5915
vineetarath <vineetarath@gmail.com>
2018-07-15 16:37:39 +0530

    fixed #4

--- 0089/BaseConv.py
+++ 0089/BaseConv.py
@@ -1,8 +1,11 @@
 remainderlist = []
-bten = int(input("Input:")) #get input
+n = int(input("Input:")) #get input
 while (remainder/2 != 0 or remainder/2 != 1):
-    for i in range(bten): #define range
-        remainder = bten%2
+    for i in range(0,bten): #define range
+        for j in range (0,i):
+            quotient = bten//2
+            remainder = bten%2
+
         print (remainder)
 print (remainderlist.append(rem))
 new = list.reverse(remainderlist)
